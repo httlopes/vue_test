a84afc503cae103c49196c968dd8d79c0987c43d
Helder Lopes <httlopes@gmail.com>
2020-12-11 20:27:34 +0000

    heroku doesnt allow https for free acounts

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = required('express');
 const serverStatic = required('serve-static');
 const history = required('connect-history-api-fallback');
-const enforce = required('express-sslify');
+//const enforce = required('express-sslify');
 
 const app = express();
 
+//app.use(enforce.HTTPS({ trustProtoHeader: true}));
+app.use(serverStatic(__dirname + '/dist'));
+app.use(history());
+
 app.listen(process.env.PORT || 5000);
+
